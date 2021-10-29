e72a05ff950295000c87517efa119c5ed2d32efd
deas <92169529+coolAIdeas@users.noreply.github.com>
2021-10-29 17:47:11 +0300

    Update index.js

--- homework_2/index.js
+++ homework_2/index.js
@@ -1,16 +1,17 @@
 let milage = prompt("Сколько километров проехала машина?");
 N = Number(milage);
-if (N === NaN) {
-	console.log("Введите корректное число!")
+if (isNaN(N)) {
+	alert("Введите корректное число!")
 } else {
 	let wheel_diameter = prompt("Какой диаметр колёс?");
 	D = Number(wheel_diameter);
-	if (D === NaN) {
-	console.log("Введите корректное число!")
+	if (isNaN(wheel_diameter)) { 
+	alert("Введите корректное число!")
 	} else {
-		let wheel_turnover = 2 * Math.PI * (D / 2)
-		wheel_turnover = Math.round(wheel_turnover);
-		alert("Одно колесо сделало "+ wheel_turnover + " оборотов. Все колёса вместе сделали: " + wheel_turnover * 4 + " оборотов");
+		let one_wheel_turnovers = (N * 1000) / (2* Math.PI*(D*0.0254)/2);
+		let all_turnovers = 4 * one_wheel_turnovers;
+		one_wheel_turnover = Math.round(one_wheel_turnovers);
+		all_turnovers = Math.round(all_turnovers);
+		alert("Одно колесо сделало "+ one_wheel_turnover + " оборотов. Все колёса вместе сделали: " + all_turnovers + " оборотов");
 	}
 }
-
